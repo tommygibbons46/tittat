0aea8c75b46de33b416dd390b84ca9b9bc253c4a
Tommy Gibbons <tommygibbons46@gmail.com>
2015-08-16 20:40:09 -0700

    Initial Commit

---
+++ TitTat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TitTat
+//
+//  Created by Thomas Gibbons on 8/16/15.
+//  Copyright (c) 2015 Thomas Gibbons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
